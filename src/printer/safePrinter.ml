ca6a92df3dad91458143f4da77a9f6ea475a00d0
Nicolas Jeannerod <nicolas.jeannerod@irif.fr>
2018-03-15 16:49:10 +0100

    fix printer

--- src/printer/safePrinter.ml
+++ src/printer/safePrinter.ml
@@ -25,6 +25,9 @@ open AST
 
 let pp_word ppf = fpf ppf "%s"
 
+let pp_word' ppf word' =
+  pp_word ppf word'.value
+                
 let rec pp_words ppf = function
   | [] -> ()
   | [word] ->
@@ -34,6 +37,10 @@ let rec pp_words ppf = function
        pp_word word
        pp_words words
 
+let pp_words' ppf words' =
+  List.map (fun word' -> word'.value) words'
+  |> pp_words ppf
+    
 let rec pp_pattern ppf = function
   | [] -> ()
   | [word] ->
@@ -43,11 +50,14 @@ let rec pp_pattern ppf = function
        pp_word word
        pp_pattern pattern
 
+let pp_pattern' ppf pattern' =
+  pp_pattern ppf pattern'.value
+    
 let pp_name ppf = fpf ppf "%s"
 
-let pp_assignment ppf (name, word) =
+let pp_assignment ppf { variable ; word } =
   fpf ppf "%a=%a"
-    pp_name name
+    pp_name variable
     pp_word word
 
 let rec pp_assignments ppf = function
@@ -59,6 +69,10 @@ let rec pp_assignments ppf = function
        pp_assignment assignment
        pp_assignments assignments
 
+let pp_assignments' ppf assignments' =
+  List.map (fun assignment' -> assignment'.value) assignments'
+  |> pp_assignments ppf
+    
 let pp_redirection_kind ppf k =
   fpf ppf "%s"
     (match k with
@@ -66,7 +80,7 @@ let pp_redirection_kind ppf k =
      | Output -> ">" | OutputDuplicate -> ">&" | OutputAppend -> ">>"
      | InputOutput -> "<>" | OutputClobber -> ">|")
 
-let rec pp_command ppf command =
+let rec pp_command ppf (command : command) =
   fpf ppf "{ ";
   (
     match command with
@@ -76,100 +90,108 @@ let rec pp_command ppf command =
 
     | Seq (command1, command2) ->
        fpf ppf "%a;%a"
-         pp_command command1
-         pp_command command2
+         pp_command' command1
+         pp_command' command2
 
     | And (command1, command2) ->
        fpf ppf "%a&&%a"
-         pp_command command1
-         pp_command command2
+         pp_command' command1
+         pp_command' command2
 
     | Or (command1, command2) ->
        fpf ppf "%a||%a"
-         pp_command command1
-         pp_command command2
+         pp_command' command1
+         pp_command' command2
 
     | Not command ->
        fpf ppf "! %a"
-         pp_command command
+         pp_command' command
 
     | Pipe (command1, command2) ->
        fpf ppf "%a|%a"
-         pp_command command1
-         pp_command command2
+         pp_command' command1
+         pp_command' command2
 
     | Subshell command ->
        fpf ppf "(%a)"
-         pp_command command
+         pp_command' command
 
-    | If (test, then_branch, None) ->
+    | If { test ; body ; rest = None } ->
        fpf ppf "if %a;then %a;fi"
-         pp_command test
-         pp_command then_branch
-    | If (test, then_branch, Some else_branch) ->
+         pp_command' test
+         pp_command' body
+
+    | If { test ; body ; rest = Some rest } ->
        fpf ppf "if %a;then %a;else %a;fi"
-         pp_command test
-         pp_command then_branch
-         pp_command else_branch
+         pp_command' test
+         pp_command' body
+         pp_command' rest
 
-    | For (name, None, body) ->
+    | For { variable ; words = None ; body } ->
        fpf ppf "for %a;do %a;done"
-         pp_name name
-         pp_command body
-    | For (name, Some words, body) ->
-       fpf ppf "for %a in %a;do %a;done"
-         pp_name name
-         pp_words words
-         pp_command body
+         pp_name variable
+         pp_command' body
 
-    | Case (word, items) ->
+    | For { variable ; words = Some words ; body } ->
+       fpf ppf "for %a in %a;do %a;done"
+         pp_name variable
+         pp_words words
+         pp_command' body
+
+    | Case { word ; items } ->
        fpf ppf "case %a in" pp_word word;
        List.iter
-         (function
-          | (pattern, None) -> fpf ppf " %a) ;;" pp_pattern pattern
-          | (pattern, Some body) -> fpf ppf " %a) %a;;" pp_pattern pattern pp_command body)
+         (fun item' ->
+           match item'.value with
+           | { pattern ; body = None } ->
+              fpf ppf " %a) ;;" pp_pattern' pattern
+           | { pattern ; body = Some body' } ->
+              fpf ppf " %a) %a;;" pp_pattern' pattern pp_command' body')
          items;
        fpf ppf "esac"
 
-    | While (test, body) ->
+    | While { test ; body } ->
        fpf ppf "while %a;do %a;done"
-         pp_command test
-         pp_command body
+         pp_command' test
+         pp_command' body
 
-    | Until (test, body) ->
+    | Until { test ; body } ->
        fpf ppf "until %a;do %a;done"
-         pp_command test
-         pp_command body
-      
-    | Function (name, body) ->
+         pp_command' test
+         pp_command' body
+
+    | Function { name ; body } ->
        fpf ppf "%a()%a"
          pp_name name
-         pp_command body
+         pp_command' body
 
-    | Simple ([], []) ->
-       failwith "SafePrinter.pp_command: ill-formed command: Simple([], [])"
-    | Simple ([], words) ->
-       fpf ppf "%a" pp_words words
-    | Simple (assignments, words) ->
+    | Simple { assignments = [] ; words = [] } ->
+       failwith "SafePrinter.pp_command': ill-formed command: Simple([], [])"
+    | Simple { assignments = [] ; words } ->
+       fpf ppf "%a" pp_words' words
+    | Simple { assignments ; words } ->
        fpf ppf "%a %a"
-         pp_assignments assignments
-         pp_words words
+         pp_assignments' assignments
+         pp_words' words
 
-    | Redirection (command, channel, kind, word) ->
+    | Redirection (command, { descr ; kind ; file }) ->
        fpf ppf "%a%s%a%a" (*WARNING: no space required only because we print a '}' at the end of each command*)
-         pp_command command
-         (match channel with None -> "" | Some channel -> string_of_int channel)
+         pp_command' command
+         (match descr with None -> "" | Some channel -> string_of_int channel)
          pp_redirection_kind kind
-         pp_word word
+         pp_word file
 
-    | HereDocument (command, channel, trim, content) ->
+    | HereDocument (command, { descr ; strip ; content }) ->
        let eof = "EOF" in (*FIXME*)
        fpf ppf "%a%s%s%s\n%a\n%s\n"
-         pp_command command
-         (match channel with None -> "" | Some channel -> string_of_int channel)
-         (if trim then "<<-" else "<<")
+         pp_command' command
+         (match descr with None -> "" | Some channel -> string_of_int channel)
+         (if strip then "<<-" else "<<")
          eof
-         pp_word content
+         pp_word' content
          eof
   );
   fpf ppf "%s}" (match command with Async _ -> "&" | HereDocument _ -> "" | _ -> ";")
+
+and pp_command' ppf (command' : command') =
+  pp_command ppf command'.value
