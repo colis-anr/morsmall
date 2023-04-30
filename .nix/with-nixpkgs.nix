50410bffa8ebe1d559cd7f7a4243b5026b043c58
Niols <niols@niols.fr>
2023-04-30 17:55:01 +0000

    Allow building with deps from nixpkgs incl Morbig

--- .nix/with-nixpkgs.nix
+++ .nix/with-nixpkgs.nix
@@ -1,35 +1,50 @@
 { ... }: {
   perSystem = { inputs', pkgs, ... }:
-    let opkgs = pkgs.ocamlPackages;
+    let
+      opkgs = pkgs.ocamlPackages;
+
+      ## Build a Morsmall package that is based on `nixpkgs`. Morbig can be
+      ## included in that treatment (with `inclMorbig = true`) in which case it
+      ## also comes from `nixpkgs`; but it can also not be (with `inclMorbig =
+      ## false`) in which case it comes from its own flake.
+      ##
+      mk-with-nixpkgs = { inclMorbig }:
+        pkgs.stdenv.mkDerivation {
+          name = "morsmall";
+          ## NOTE: The use of `./..` matters because the path is taken as relative to
+          ## the current file, and therefore to `.nix/`.
+          src = ./..;
+
+          nativeBuildInputs = with opkgs; [
+            ## Basic ones, always necessary
+            ocaml
+            dune_3
+            findlib
+          ];
+
+          propagatedBuildInputs = with opkgs; [
+            (if inclMorbig then
+              morbig
+            else
+              inputs'.morbig.packages.with-nixpkgs)
+
+            ppx_deriving
+            ppx_deriving_yojson
+            visitors
+            yojson
+          ];
+
+          buildPhase = ''
+            make build
+          '';
+
+          installPhase = ''
+            make install PREFIX=$out
+          '';
+        };
     in {
-      packages.with-nixpkgs = pkgs.stdenv.mkDerivation {
-        name = "morsmall";
-        ## NOTE: The use of `./..` matters because the path is taken as relative to
-        ## the current file, and therefore to `.nix/`.
-        src = ./..;
-
-        nativeBuildInputs = with opkgs; [
-          ## Basic ones, always necessary
-          ocaml
-          dune_3
-          findlib
-        ];
-
-        propagatedBuildInputs = with opkgs; [
-          inputs'.morbig.packages.with-nixpkgs
-          ppx_deriving
-          ppx_deriving_yojson
-          visitors
-          yojson
-        ];
-
-        buildPhase = ''
-          make build
-        '';
-
-        installPhase = ''
-          make install PREFIX=$out
-        '';
-      };
+      packages.with-nixpkgs = mk-with-nixpkgs { inclMorbig = false; };
+      packages.with-nixpkgs-incl-morbig =
+        mk-with-nixpkgs { inclMorbig = true; };
     };
 }
