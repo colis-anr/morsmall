ee81cbf61ecb5d2ae20026348b4ea126a119cfda
Niols <niols@niols.fr>
2023-05-01 00:34:32 +0000

    No need to fix OCamlFind's version anymore

--- .nix/with-opam-nix.nix
+++ .nix/with-opam-nix.nix
@@ -7,13 +7,6 @@
         inherit pkgs;
         resolveArgs.with-doc = true;
         resolveArgs.with-test = true;
-      } "morsmall" ../. {
-        ocaml-base-compiler = "*";
-
-        ## FIXME: Somehow, as of 2023-04-10, 1.9.6 cannot compile. So in the
-        ## meantime we specify a specific version here. We should relax this
-        ## as soon as things get working again.
-        ocamlfind = "1.9.5";
-      };
+      } "morsmall" ../. { ocaml-base-compiler = "*"; };
     in { packages.with-opam-nix = scope.morsmall // { inherit scope; }; };
 }
