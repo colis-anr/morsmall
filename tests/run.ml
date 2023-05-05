5bc0daf9e02252c1a0a0403272157ec74b23c6c7
Niols <niols@niols.fr>
2023-05-05 14:57:49 +0000

    Full rework of the tests runner

--- tests/run.ml
+++ tests/run.ml
@@ -1,5 +1,6 @@
 let pf = Format.printf
 let fpf = Format.fprintf
+let spf = Format.sprintf
 
 exception CouldntParse of Morsmall.AST.program
 exception ASTsDontMatch of Morsmall.AST.program * Morsmall.AST.program
@@ -8,103 +9,82 @@ let generator_parameters = Generator.default_parameters
 
 let number_of_tests = 100
 
-(* *)
+let artifacts_directory_prefix =
+  "artifacts"
 
-let run_one_test () =
-  (* Create a temporary file *)
-  let (filename, out_channel) = Filename.open_temp_file "morsmall_test_" ".sh" in
-  let formatter = Format.formatter_of_out_channel out_channel in
+let create_artifacts_directory_prefix_if_not_exist () =
+  if not Sys.(file_exists artifacts_directory_prefix
+              && is_directory artifacts_directory_prefix) then
+    Unix.mkdir artifacts_directory_prefix 0o777
 
-  (* Create a random script, put it in the file *)
-  let ast = Generator.(g_program generator_parameters) in
-  Morsmall.pp_print_safe formatter ast;
+let artifacts_directory ~test_number =
+  spf "%s/%d" artifacts_directory_prefix test_number
 
-  (* Close the file *)
-  fpf formatter "@.";
-  close_out out_channel;
+let create_artifacts_directory ~test_number =
+  create_artifacts_directory_prefix_if_not_exist ();
+  Unix.mkdir (artifacts_directory ~test_number) 0o777
 
+let artifacts_file ~test_number artifact_name =
+  Filename.concat (artifacts_directory ~test_number) artifact_name
+
+let with_file fname cont =
+  let ochan = open_out fname in
+  let fmt = Format.formatter_of_out_channel ochan in
   try
-    (
-      (* Parse the file with Morbig and Morsmall *)
-      let ast' =
-        try
-          Morsmall.parse_file filename
-        with
-          Morsmall.SyntaxError _pos ->
-          raise (CouldntParse ast)
-      in
-
-      (* Compare *)
-      if not (Morsmall.AST.equal_program ast ast') then
-           raise (ASTsDontMatch (ast, ast'))
-    );
-
-    (* Clean the temporary file *)
-    Sys.remove filename
+    let res = cont fmt in
+    Format.pp_print_flush fmt ();
+    Stdlib.close_out ochan;
+    res
   with
-  | _ as exn ->
-     Sys.remove filename;
-     raise exn
+    exn ->
+    Format.pp_print_flush fmt ();
+    Stdlib.close_out ochan;
+    raise exn
+
+let run_one_test ~test_number =
+  create_artifacts_directory ~test_number;
+
+  let input = Generator.(g_program generator_parameters) in
+
+  let fname = artifacts_file ~test_number "input.show" in
+  with_file fname (fun fmt -> Morsmall.pp_print_debug fmt input);
+
+  let fname = artifacts_file ~test_number "input.sh" in
+  with_file fname (fun fmt -> Morsmall.pp_print_safe fmt input);
+
+  let output =
+    try Morsmall.parse_file fname
+    with Morsmall.SyntaxError _pos -> raise (CouldntParse input)
+  in
+
+  let fname = artifacts_file ~test_number "output.show" in
+  with_file fname (fun fmt -> Morsmall.pp_print_debug fmt output);
+
+  if not (Morsmall.AST.equal_program input output) then
+    raise (ASTsDontMatch (input, output))
 
 let () =
+  pf "@.";
   let errors = ref 0 in
-
-  for i = 1 to number_of_tests do
-    pf "Running test #%d...\r@?" i;
+  for test_number = 1 to number_of_tests do
+    pf "Running test #%d...\r@?" test_number;
     try
-      run_one_test ()
+      run_one_test ~test_number
     with
-    | _ as exn ->
-       (
-         (* Generate a report *)
-
-         let filename = "morsmall_test_report_"^(string_of_int i)^".org" in
-         let out_channel = open_out filename in
-         let formatter = Format.formatter_of_out_channel out_channel in
-         fpf formatter "#+TITLE: Morsmall Test Engine -- Report on Test #%d\n\n" i;
-         fpf formatter "* The Error\n";
-         let ast =
-           (
-             match exn with
-             | CouldntParse ast ->
-                fpf formatter "Morbig could not parse the file produced by Morsmall's printer.\n";
-                ast
-
-             | ASTsDontMatch (ast, _) ->
-                fpf formatter "The parsed AST does not coincide with the generated one.\n";
-                ast
-
-             | _ as exn -> raise exn
-           )
-         in
-         fpf formatter "* Generated AST\n%a\n" Morsmall.pp_print_debug ast;
-         fpf formatter "* Generated Shell Script\n%a\n" Morsmall.pp_print_safe ast;
-         (
-           match exn with
-           | ASTsDontMatch (_, ast') ->
-              (
-                fpf formatter
-                  "* Parsed AST\n%a\n"
-                  Morsmall.pp_print_debug ast';
-
-                try
-                  fpf formatter
-                    "* Parsed Shell Script\n%a\n"
-                    Morsmall.pp_print_safe ast'
-                with
-                  Assert_failure _ ->
-                  fpf formatter "* Error while printing the parsed AST@.";
-                  Printexc.print_backtrace out_channel
-              )
-           | _ -> ()
-         );
-         fpf formatter "@?";
-         close_out out_channel;
-
-         (* Complain *)
-         Format.eprintf "Error in test #%d: check report in '%s'\n" i filename;
-         incr errors
-       )
+      exn ->
+      (
+        incr errors;
+        pf "Error while running test #%d.@." test_number;
+        (
+          match exn with
+          | CouldntParse _input ->
+            pf "Morbig could not parse the file produced by Morsall's printer.@."
+          | ASTsDontMatch (_input, _output) ->
+            pf "The input and output ASTs do not match.@."
+          | exn -> raise exn
+        );
+        pf "@."
+      )
   done;
 
   if !errors = 0 then
